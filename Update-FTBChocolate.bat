cad0a8e35356b0ff50df38ca8a86f1ecb689e467
chazza6464 <107543768+chazza6464@users.noreply.github.com>
2022-12-08 23:29:37 +0000

    FTB Chocolate - Upload BATCH
    Uploaded Update-FTBChocolate.bat

---
+++ Update-FTBChocolate.bat
@@ -0,0 +1,3 @@
+@echo off
+curl https://github.com/chazza6464/Modpacks/blob/main/Update-FTBChocolate.ps1 output Update-FTBChocolate.ps1
+powershell.exe -ExecutionPolicy Bypass -File .\Update-FTBChocolate.ps1
