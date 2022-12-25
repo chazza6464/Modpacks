9250284fe9fe162932dc3f4959b523185ac9d74f
chazza6464 <107543768+chazza6464@users.noreply.github.com>
2022-12-25 19:43:10 +0000

    Pixelmon - Final bugfixes

--- Update-Pixelmon.ps1
+++ Update-Pixelmon.ps1
@@ -2,6 +2,7 @@
 
 # Stage Zero - Setup
 $ErrorActionPreference = "SilentlyContinue"
+$ProgressPreference = "SilentlyContinue"
 Add-Type -AssemblyName System.Windows.Forms
 
 # Stage One - Identify Instance Folder
@@ -19,7 +20,7 @@ if (Test-Path "$env:USERPROFILE\Desktop\MultiMC\Instances") {
 } else {
     Write-Host "Could not find MultiMC or Pixelmon in the standard location." -ForegroundColor Yellow
     $Picker = New-Object System.Windows.Forms.FolderBrowserDialog
-    $Picker.Description = "Select the MultiMC folder"
+    $Picker.Description = "Select the MultiMC > Instances > Pixelmon > Minecraft folder"
     $Picker.InitialDirectory = "$env:USERPROFILE\Desktop"
     $Picker.TopMost = $true
     
@@ -37,23 +38,23 @@ if (Test-Path "$env:USERPROFILE\Desktop\MultiMC\Instances") {
 Write-Host "`nStage Two - Installing additional mods to Pixelmon..." -ForegroundColor Cyan
 
 # Create
-Invoke-WebRequest -Uri "https://github.com/chazza6464/Modpacks/raw/main/Mod%20Files/create-mc1.16.5_v0.3.2g.jar" -OutFile "$InstancePath\Mods\create-mc1.16.5_v0.3.2g.jar"
+Invoke-WebRequest -Uri "https://github.com/chazza6464/Modpacks/raw/main/Mod%20Files/create-mc1.16.5_v0.3.2g.jar" -OutFile "$InstancePath\mods\create-mc1.16.5_v0.3.2g.jar"
 Write-Host "(1/5) Downloaded Create" -ForegroundColor Yellow
 
 # Flywheel
-Invoke-WebRequest -Uri "https://github.com/chazza6464/Modpacks/raw/main/Mod%20Files/flywheel-1.16-0.2.5.jar" -OutFile "$InstancePath\Mods\flywheel-1.16.0.2.5.jar"
+Invoke-WebRequest -Uri "https://github.com/chazza6464/Modpacks/raw/main/Mod%20Files/flywheel-1.16-0.2.5.jar" -OutFile "$InstancePath\mods\flywheel-1.16.0.2.5.jar"
 Write-Host "(2/5) Downloaded Flywheel" -ForegroundColor Yellow
 
 # JEI
-Invoke-WebRequest -Uri "https://github.com/chazza6464/Modpacks/raw/main/Mod%20Files/jei-1.16.5-7.7.1.153.jar" -OutFile "$InstancePath\Mods\jei-1.16.5-7.7.1.153.jar"
+Invoke-WebRequest -Uri "https://github.com/chazza6464/Modpacks/raw/main/Mod%20Files/jei-1.16.5-7.7.1.153.jar" -OutFile "$InstancePath\mods\jei-1.16.5-7.7.1.153.jar"
 Write-Host "(3/5) Downloaded JEI" -ForegroundColor Yellow
 
 # OreExcavation
-Invoke-WebRequest -Uri "https://github.com/chazza6464/Modpacks/raw/main/Mod%20Files/OreExcavation-1.8.157.jar" -OutFile "$InstancePath\Mods\OreExcavation-1.8.157.jar"
+Invoke-WebRequest -Uri "https://github.com/chazza6464/Modpacks/raw/main/Mod%20Files/OreExcavation-1.8.157.jar" -OutFile "$InstancePath\mods\OreExcavation-1.8.157.jar"
 Write-Host "(4/5) Downloaded OreExcavation" -ForegroundColor Yellow
 
 # Simple Storage Network
-Invoke-WebRequest -Uri "https://github.com/chazza6464/Modpacks/raw/main/Mod%20Files/SimpleStorageNetwork-1.16.5-1.5.2.jar" -OutFile "$InstancePath\Mods\SimpleStorageNetwork-1.16.5-1.5.2.jar"
+Invoke-WebRequest -Uri "https://github.com/chazza6464/Modpacks/raw/main/Mod%20Files/SimpleStorageNetwork-1.16.5-1.5.2.jar" -OutFile "$InstancePath\mods\SimpleStorageNetwork-1.16.5-1.5.2.jar"
 Write-Host "(5/5) Downloaded Simple Storage Network" -ForegroundColor Yellow
 
 Write-Host "All mods and updates have been downloaded to the Pixelmon mod!" -ForegroundColor Green
