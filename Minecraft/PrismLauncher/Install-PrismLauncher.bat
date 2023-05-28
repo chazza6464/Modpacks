92107c6871f85188dde98d8fa89673313d40d1c1
chazza6464 <107543768+chazza6464@users.noreply.github.com>
2023-05-28 14:37:47 +0100

    Bug Fix
    Corrected URL

--- Minecraft/PrismLauncher/Install-PrismLauncher.bat
+++ Minecraft/PrismLauncher/Install-PrismLauncher.bat
@@ -1,4 +1,4 @@
-powershell.exe -Command "Invoke-WebRequest -Uri https://raw.githubusercontent.com/chazza6464/Modpacks/main/PrismLauncher/Install-PrismLauncher.ps1 -OutFile .\Install-PrismLauncher.ps1" & cls
+powershell.exe -Command "Invoke-WebRequest -Uri https://github.com/chazza6464/Modpacks/raw/main/Minecraft/PrismLauncher/Install-PrismLauncher.ps1 -OutFile .\Install-PrismLauncher.ps1" & cls
 powershell.exe -ExecutionPolicy Bypass -STA -File .\Install-PrismLauncher.ps1
 DEL .\Install-PrismLauncher.ps1
 pause
