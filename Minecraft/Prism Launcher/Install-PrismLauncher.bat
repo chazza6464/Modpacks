07752a167d879a9973123f85792a63e84c21b66b
chazza6464 <107543768+chazza6464@users.noreply.github.com>
2023-05-28 14:35:13 +0100

    Bug Fix
    Removed space from subfolder "Prism Launcher"

--- Minecraft/Prism Launcher/Install-PrismLauncher.bat
+++
@@ -1,4 +0,0 @@
-powershell.exe -Command 'Invoke-WebRequest -Uri "https://raw.githubusercontent.com/chazza6464/Modpacks/main/Prism Launcher/Install-PrismLauncher.ps1" -OutFile .\Install-PrismLauncher.ps1' & cls
-powershell.exe -ExecutionPolicy Bypass -STA -File .\Install-PrismLauncher.ps1
-DEL .\Install-PrismLauncher.ps1
-pause
